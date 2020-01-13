97a3940edf00e5dca1f3d82a65206fe4aeded55b
Nathan Bittner <njbittner@gmail.com>
2020-01-12 19:10:56 -0800

    Minor cleanup in FightView

--- views/FightView.py
+++ views/FightView.py
@@ -1,36 +1,25 @@
 import arcade
 from Player import Player
-import math
 import random
 from constants import *
 import glob
 
-"""
-Need to have Start and End ANimation
-"""
 
-## MAIN GAME CLASS
 class FightView(arcade.View):
-
     def __init__(self, window):
         """
         :param width: Width of Window
         :param height:  Height of window
         :param title:  Title of game
         """
-        # Call the parent class's init function
         super().__init__()
 
-        # Make the mouse disappear when it is over the window.
-        # So we just see our object, not the pointer.
         self.window = window
         self.window.set_mouse_visible(False)
 
-        # arcade.set_background_color(arcade.color.ASH_GREY)
-
         # Create our players
-        self.player1 = Player(int(1/8 * SCREEN_WIDTH), Y_BASELINE, BOX_WIDTH, BOX_HEIGHT, 0, character='jason')
-        self.player2 = Player(int(7/8 * SCREEN_WIDTH), Y_BASELINE, BOX_WIDTH, BOX_HEIGHT, 1, character='ian')
+        self.player1 = Player(int(1/8 * SCREEN_WIDTH), Y_BASELINE, BOX_WIDTH, BOX_HEIGHT, 0, character='nate')
+        self.player2 = Player(int(7/8 * SCREEN_WIDTH), Y_BASELINE, BOX_WIDTH, BOX_HEIGHT, 1, character='nate')
 
         # endgame
         self.winner = None
@@ -40,18 +29,15 @@ class FightView(arcade.View):
         bg_image = random.choice(bg_images)
         self.background_texture = arcade.load_texture(bg_image)
 
-        # Saving recordings
-        self.abs_counter = 0
-
         # background music
-        self.enable_bg = True
-        songs = glob.glob(os.path.join(MUSIC_ROOT, "*.wav"))
-        bg_song = random.choice(songs)
-        self.background_music = arcade.load_sound(bg_song)
-        if self.enable_bg:
+        self.enable_bg_music = True
+        if self.enable_bg_music:
+            songs = glob.glob(os.path.join(MUSIC_ROOT, "*.wav"))
+            bg_song = random.choice(songs)
+            self.background_music = arcade.load_sound(bg_song)
             arcade.play_sound(self.background_music)
 
-        # Do a countdown
+        # Initial Countdown
         self.countdown_counter = (COUNTDOWN_FROM + 1) * TICKS_PER_COUNTDOWN
         self.countdown_values = range(1, COUNTDOWN_FROM+1)
 
@@ -71,18 +57,15 @@ class FightView(arcade.View):
         if self.player2.health <1:
             self.winner = self.player1
 
-        if self.winner and self.enable_bg:
+        if self.winner and self.enable_bg_music:
             self.player1.game_over = True
             self.player2.game_over = True
-            # self.background_music.player.pause()
-            # arcade.stop_sound(self.background_music)
 
     def on_update(self, delta_time):
-        """ Initial Update """
-
         if self.countdown_counter:
             self.countdown_counter -= 1
             if self.countdown_counter == TICKS_PER_COUNTDOWN:
+                # set the players so they're ready to go
                 self.player1.state = ('medium', 'idle')
                 self.player2.state = ('medium', 'idle')
         else:
@@ -106,16 +89,20 @@ class FightView(arcade.View):
         # Draw background
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background_texture)
+        # Draw players
         self.player1.draw()
         self.player2.draw()
 
+        # Draw countdown timer
         if self.countdown_counter > TICKS_PER_COUNTDOWN:
             arcade.draw_text(f"{self.countdown_values[(self.countdown_counter-TICKS_PER_COUNTDOWN) // TICKS_PER_COUNTDOWN]}", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
                              arcade.color.RED, font_size=120, anchor_x="center", font_name="Utopia", bold=True)
+        # Draw "Fight" message
         elif self.countdown_counter:
             arcade.draw_text(FIGHT_MSG,
                              SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
                              arcade.color.RED, font_size=120, anchor_x="center", font_name="Utopia", bold=True)
+        # Draw "Winner message
         elif self.winner:
             arcade.draw_text(f"Player {self.winner.playerIdx + 1} WINS!!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
                              arcade.color.RED, font_size=60, anchor_x="center", font_name="Utopia")
